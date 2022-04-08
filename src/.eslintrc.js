5701b83c292a1bd9e0b2494cf7ddf22b37af6587
Elmar <L-mar@inbox.ru>
2022-04-08 23:26:40 +0400

    Add .eslintrc.js file

---
+++ src/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "env": {
+      "browser": true,
+      "es2021": true,
+      "node": true
+  },
+  "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended"
+  ],
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": "latest",
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react"
+  ],
+  "rules": {
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+  }
+}
