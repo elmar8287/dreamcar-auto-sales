27be8fa5ed939cca8053641f7012e3df1b8d8413
Elmar <L-mar@inbox.ru>
2022-04-09 18:50:21 +0400

    Create a cards

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../Header/Header";
 import Hiro from "../Hiro/Hiro";
+import Inventory from "../Inventory/Inventory";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Hiro />
+      <Inventory />
     </div>
   );
 }
