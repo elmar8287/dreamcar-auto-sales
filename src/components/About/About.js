a0a542fe01d9d407dac0b502a4fc4152581ed256
Elmar <L-mar@inbox.ru>
2022-04-11 09:54:12 +0400

    Create About section and make it responsive

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div>
+      <div className="about" id="about">
+        <h2>About us</h2>
+        <p>Lorem ipsum</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
