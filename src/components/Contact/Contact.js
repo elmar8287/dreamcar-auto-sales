c4d03f7c03b1cca1e81b16dc7f630994871e49d1
Elmar <L-mar@inbox.ru>
2022-05-14 17:17:02 +0400

    Save input values into localStorage

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import {data} from "../../utils/localStorage";
 import "./Contact.css";
 
 function Contact() {
-  const [value, setValue] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const inputHandle = (e) => {
-    e.preventDefault();
-    setValue(e.target.value);
+  // Get data from localStorage with condition of empty storage
+  const getData = () => {
+    const storeData = localStorage.getItem('data')
+    if (!storeData) {
+      return {
+        name: "",
+        email: "",
+        message: "",
+      }
+    } else {
+      return JSON.parse(storeData)
+    }
+  }
+  // Setup state and monitor any updates of it.
+  const [value, setValue] = useState(getData);
+  useEffect(()=>{
+    data(value);
+  },[value])
+  
+  const nameHandle = (e) => {
+    setValue({...value, name: e.target.value});
+  };
+  const emailHandle = (e) => {
+    setValue({...value, email: e.target.value});
+  };
+  const messageHandle = (e) => {
+    setValue({...value, message: e.target.value});
   };
   const [submited, setSubmited] = useState(false);
   const submitedHandle = () => {
@@ -19,9 +39,9 @@ function Contact() {
   <div className="contact-form" id="contact">
     <h2>Leave your message</h2>
     <form action="https://formspree.io/f/mbjwjnay" method="POST" onSubmit={submitedHandle}>
-      <input type="text" name="name" placeholder="Your name.." required value={value.name} onChange={inputHandle}/>
-      <input type="email" name="email" placeholder="Your mail address.." required value={value.email} onChange={inputHandle}/>
-      <textarea name="message" placeholder="Your message.." required value={value.message} onChange={inputHandle}/>
+      <input type="text" name="name" placeholder="Your name.." required value={value.name} onChange={nameHandle}/>
+      <input type="email" name="email" placeholder="Your mail address.." required value={value.email} onChange={emailHandle}/>
+      <textarea name="message" placeholder="Your message.." required value={value.message} onChange={messageHandle}/>
       <input type="submit" value="Send"/>
       {
         submited ? 
