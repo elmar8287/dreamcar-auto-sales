c7d6d289d2cdf8a074469a233cf4c51e0be43b3a
Elmar <L-mar@inbox.ru>
2022-05-14 23:24:19 +0400

    Refactor the action dispatchs by adding the binActionCreators

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,10 +1,11 @@
-import { createStore } from "react-redux";
-
-import { nameHandle, emailHandle, messageHandle } from "../redux/actions";
+import { createStore, binActionCreators } from "react-redux";
+import * as actions from "../redux/actions";
 import reducer from "../redux/reducer";
 
 const store = createStore(reducer);
+const { dispatch } = store;
+const { nameHandle, emailHandle, messageHandle } = binActionCreators(actions, dispatch);
 
-store.dispatch(nameHandle());
-store.dispatch(emailHandle());
-store.dispatch(messageHandle());
+dispatch(nameHandle());
+dispatch(emailHandle());
+dispatch(messageHandle());
