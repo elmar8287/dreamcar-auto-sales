65b810d9f306a7a18177318d6bd9252fb2f2cfd8
Elmar <L-mar@inbox.ru>
2022-07-01 19:50:26 +0400

    Fix the sorting

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -18,72 +18,6 @@ const Inventory = () => {
     <div className="car-block" id="cars">
       <h2 className="car-list-header">Latest updates ({carNumber} cars)</h2>
       <div className="car-list">
-      <div className="card">
-          <img src={sprinter3} />
-          <div className="car-container">
-            <h4>Thor citation Mercedes</h4>
-            <p className="price">89.995$</p>
-            <ul>
-              <li>Year: 2016</li>
-              <li>Condition: Like new</li>
-              <li>Cylinders: 6 cylinders</li>
-              <li>Transmission: Automatic</li>
-              <li>Fuel: Diesel</li>
-              <li>Odometer: 47000</li>
-            </ul>
-            <button type='button' className="card-button"><a href="https://seattle.craigslist.org/see/rvd/d/kirkland-2016-thor-citation-mercedes/7484841063.html" target="_blank" rel="noreferrer">Detail</a></button>
-          </div>
-        </div>
-        <div className="card">
-          <img src={sprinter} />
-          <div className="car-container">
-            <h4>Winnebago view sprinter</h4>
-            <p className="price">49.995$</p>
-            <ul>
-              <li>Year: 2008</li>
-              <li>Condition: Like new</li>
-              <li>Cylinders: 6 cylinders</li>
-              <li>Transmission: Automatic</li>
-              <li>Fuel: Diesel</li>
-              <li>Odometer: 109000</li>
-            </ul>
-            <button type='button' className="card-button"><a href="https://seattle.craigslist.org/see/rvd/d/kirkland-2008-winnebago-view-sprinter/7480695217.html" target="_blank" rel="noreferrer">Detail</a></button>
-          </div>
-        </div>
-        <div className="card">
-          <img src={sprinter4} />
-          <div className="car-container">
-            <h4>Renovated leisure vans Mercedes sprinter</h4>
-            <p className="price">45.000$</p>
-            <ul>
-              <li>Year: 2007</li>
-              <li>Condition: Good</li>
-              <li>Cylinders: 6 cylinders</li>
-              <li>Transmission: Automatic</li>
-              <li>Fuel: Diesel</li>
-              <li>Odometer: 144000</li>
-            </ul>
-              <button type='button' className="card-button"><a href="https://vanlifetrader.com/listing/renovated-leisure-vans-mercedes-sprinter/" target="_blank" rel="noreferrer">Detail</a></button>
-          </div>
-        </div>
-        <div className="card">
-          <img src={sprinter5} />
-          <div className="car-container">
-            <h4>Low miles pre-def sprinter</h4>
-            <p className="price">79.995$</p>
-            <ul>
-              <li>Year: 2007</li>
-              <li>Condition: Good</li>
-              <li>Cylinders: 6 cylinders</li>
-              <li>Transmission: Automatic</li>
-              <li>Fuel: Diesel</li>
-              <li>Odometer: 23000</li>
-            </ul>
-              <button type='button' className="card-button"><a href="https://vanlifetrader.com/listing/new-professional-build-low-miles-pre-def-sprinter/" target="_blank" rel="noreferrer">Detail</a></button>
-          </div>
-        </div>
-      </div>
-      <div className="car-list">
         <div className="card">
           <img src={sprinter6} />
           <div className="car-container">
@@ -146,6 +80,72 @@ const Inventory = () => {
               <li>Odometer: 112000</li>
             </ul>
               <button type='button' className="card-button"><a href="https://seattle.craigslist.org/see/ctd/d/kirkland-2008-mercedes-sprinter-four/7492862186.html" target="_blank" rel="noreferrer">Detail</a></button>
+          </div>
+        </div>
+      </div>
+      <div className="car-list">
+      <div className="card">
+          <img src={sprinter3} />
+          <div className="car-container">
+            <h4>Thor citation Mercedes</h4>
+            <p className="price">89.995$</p>
+            <ul>
+              <li>Year: 2016</li>
+              <li>Condition: Like new</li>
+              <li>Cylinders: 6 cylinders</li>
+              <li>Transmission: Automatic</li>
+              <li>Fuel: Diesel</li>
+              <li>Odometer: 47000</li>
+            </ul>
+            <button type='button' className="card-button"><a href="https://seattle.craigslist.org/see/rvd/d/kirkland-2016-thor-citation-mercedes/7484841063.html" target="_blank" rel="noreferrer">Detail</a></button>
+          </div>
+        </div>
+        <div className="card">
+          <img src={sprinter} />
+          <div className="car-container">
+            <h4>Winnebago view sprinter</h4>
+            <p className="price">49.995$</p>
+            <ul>
+              <li>Year: 2008</li>
+              <li>Condition: Like new</li>
+              <li>Cylinders: 6 cylinders</li>
+              <li>Transmission: Automatic</li>
+              <li>Fuel: Diesel</li>
+              <li>Odometer: 109000</li>
+            </ul>
+            <button type='button' className="card-button"><a href="https://seattle.craigslist.org/see/rvd/d/kirkland-2008-winnebago-view-sprinter/7480695217.html" target="_blank" rel="noreferrer">Detail</a></button>
+          </div>
+        </div>
+        <div className="card">
+          <img src={sprinter4} />
+          <div className="car-container">
+            <h4>Renovated leisure vans Mercedes sprinter</h4>
+            <p className="price">45.000$</p>
+            <ul>
+              <li>Year: 2007</li>
+              <li>Condition: Good</li>
+              <li>Cylinders: 6 cylinders</li>
+              <li>Transmission: Automatic</li>
+              <li>Fuel: Diesel</li>
+              <li>Odometer: 144000</li>
+            </ul>
+              <button type='button' className="card-button"><a href="https://vanlifetrader.com/listing/renovated-leisure-vans-mercedes-sprinter/" target="_blank" rel="noreferrer">Detail</a></button>
+          </div>
+        </div>
+        <div className="card">
+          <img src={sprinter5} />
+          <div className="car-container">
+            <h4>Low miles pre-def sprinter</h4>
+            <p className="price">79.995$</p>
+            <ul>
+              <li>Year: 2007</li>
+              <li>Condition: Good</li>
+              <li>Cylinders: 6 cylinders</li>
+              <li>Transmission: Automatic</li>
+              <li>Fuel: Diesel</li>
+              <li>Odometer: 23000</li>
+            </ul>
+              <button type='button' className="card-button"><a href="https://vanlifetrader.com/listing/new-professional-build-low-miles-pre-def-sprinter/" target="_blank" rel="noreferrer">Detail</a></button>
           </div>
         </div>
       </div>
