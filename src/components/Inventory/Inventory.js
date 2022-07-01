e783c1e8e0a78ac69405f69e9820e34bb52bb037
Elmar <L-mar@inbox.ru>
2022-07-01 19:37:30 +0400

    Add new cars

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -9,6 +9,9 @@ const sprinter3 = require("./sprinter3.png");
 const sprinter4 = require("./sprinter4.png");
 const sprinter5 = require("./sprinter5.png");
 const sprinter6 = require("./sprinter6.png");
+const sprinter7 = require("./sprinter7.png");
+const sprinter8 = require("./sprinter8.png");
+const sprinter9 = require("./sprinter9.png");
 
 const Inventory = () => {
   const carNumber = useSelector((state)=>state);
@@ -99,19 +102,51 @@ const Inventory = () => {
           </div>
         </div>
         <div className="card">
-          <img src={sprinter2} />
+          <img src={sprinter7} />
           <div className="car-container">
-            <h4>Mercedes Sprinter 2014</h4>
-            <p className="price">25.995$</p>
+            <h4>Thor Citation Mercedes Sprinter</h4>
+            <p className="price">79.995$</p>
+            <ul>
+              <li>Year: 2016</li>
+              <li>Condition: Like new</li>
+              <li>Cylinders: 6 cylinders</li>
+              <li>Transmission: Automatic</li>
+              <li>Fuel: Diesel</li>
+              <li>Odometer: 47000</li>
+            </ul>
+              <button type='button' className="card-button"><a href="https://seattle.craigslist.org/see/rvd/d/kirkland-2016-thor-citation-mercedes/7498016513.html" target="_blank" rel="noreferrer">Detail</a></button>
+          </div>
+        </div>
+        <div className="card">
+          <img src={sprinter8} />
+          <div className="car-container">
+            <h4>Mercedes Sprinter Cargo VAN</h4>
+            <p className="price">24.995$</p>
             <ul>
               <li>Year: 2014</li>
+              <li>Condition: Good</li>
+              <li>Cylinders: 4 cylinders</li>
+              <li>Transmission: Automatic</li>
+              <li>Fuel: Diesel</li>
+              <li>Odometer: 180000</li>
+            </ul>
+              <button type='button' className="card-button"><a href="https://seattle.craigslist.org/see/ctd/d/kirkland-2014-mercedes-sprinter-diesel/7496303349.html" target="_blank" rel="noreferrer">Detail</a></button>
+          </div>
+        </div>
+        <div className="card">
+          <img src={sprinter9} />
+          <div className="car-container">
+            <h4>Mercedes Sprinter Four Winds Camper</h4>
+            <p className="price">45.000$</p>
+            <ul>
+              <li>Year: 2008</li>
               <li>Condition: Good</li>
               <li>Cylinders: 6 cylinders</li>
               <li>Transmission: Automatic</li>
               <li>Fuel: Diesel</li>
-              <li>Odometer: 120000</li>
+              <li>Odometer: 112000</li>
             </ul>
-              <button type='button' className="sold"><a href="https://seattle.craigslist.org/see/ctd/d/kirkland-2014-mercedes-sprinter-2500/7463823261.html" target="_blank" rel="noreferrer">Sold</a></button>
+              <button type='button' className="card-button"><a href="https://seattle.craigslist.org/see/ctd/d/kirkland-2008-mercedes-sprinter-four/7492862186.html" target="_blank" rel="noreferrer">Detail</a></button>
           </div>
         </div>
       </div>
