e6002b05f99dac772cb36bf649497b97739ad2ba
Elmar <L-mar@inbox.ru>
2022-07-09 12:34:19 +0400

    Fix linter issues

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -17,7 +17,7 @@ const Inventory = () => {
         {
           carsList.map(car=>(
             <div className="card" key={car.id}>
-              <img src={car.img} onError={(e)=>{e.target.onerror = null; e.target.src="https://img.freepik.com/free-vector/sold-out-price-tag-sign_123447-162.jpg"}} alt={car.model}/>;
+              <img src={car.img} onError={(e)=>{e.target.onerror = null; e.target.src="https://img.freepik.com/free-vector/sold-out-price-tag-sign_123447-162.jpg"}} alt={car.model} />
               <div className="car-container">
                 <h4>{car.model}</h4>
                 <p className="price">{car.price}$</p>
