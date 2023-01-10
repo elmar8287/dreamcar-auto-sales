076f8363bd5a4dbd8aa8cb08b7b1d8d56f2efdb2
Elmar Abdulkarimov <49064106+elmar8287@users.noreply.github.com>
2023-01-10 20:02:01 +0000

    Added the partners in nav bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,7 +15,8 @@ const Header = () => {
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#cars">Cars</Nav.Link>
-            <Nav.Link href="#contact">Contact </Nav.Link>
+            <Nav.Link href="#partners">Partners</Nav.Link>
+
           </Nav>
         </Navbar.Collapse>
         
