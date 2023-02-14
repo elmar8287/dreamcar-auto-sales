3d8e59efdd10cbcddd542e983c269b24f35528c9
Elmar Abdulkarimov <49064106+elmar8287@users.noreply.github.com>
2023-02-14 19:59:53 +0000

    Make about section construction

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,10 +3,15 @@ import "./About.css";
 
 const About = () => {
   return (
-    <div>
+    <div className="about-main">
       <div className="about" id="about">
         <h2>About us</h2>
-        <p>Here at Dreamcar, we specialized in diesel cargo vans.
+        <p>
+        Here at Dreamcar, we specialized in diesel cargo vans.
+        </p>
+      </div>
+      <div className="about-text">
+        <p>
           We carefully select them for you!
           Every van goes through extensive mechanical preparations and service at our partner
           <a className="about-href" href="https://www.google.com/maps/place/Sprinter+Repair+and+Parts/@47.7709884,-122.148323,15z/data=!4m5!3m4!1s0x0:0x1c1692d52afa4704!8m2!3d47.7709884!4d-122.148323?hl=en">
